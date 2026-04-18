7e39ca1f9f753f5f8ebc8646e0edf2243c9484fe
ThibLOREK <thibaudlorek@outlook.com>
2026-04-19 00:26:22 +0200

    fix: BrowserRouter manquant + suppression App.jsx/api.js anciens

--- web/ui/src/App.tsx
+++ web/ui/src/App.tsx
@@ -1,20 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Layout from '@/components/Layout'
-import ProjectsPage from '@/pages/ProjectsPage'
-import EditorPage from '@/pages/EditorPage'
-import ConnectionsPage from '@/pages/ConnectionsPage'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ProjectsPage from './pages/ProjectsPage'
+import EditorPage from './pages/EditorPage'
+import ConnectionsPage from './pages/ConnectionsPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/projects" replace />} />
-          <Route path="projects" element={<ProjectsPage />} />
-          <Route path="projects/:projectId/edit" element={<EditorPage />} />
-          <Route path="connections" element={<ConnectionsPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/projects" replace />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/projects/:projectId/edit" element={<EditorPage />} />
+      <Route path="/connections" element={<ConnectionsPage />} />
+    </Routes>
   )
 }
